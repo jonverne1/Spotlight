21b14a30acad6db5e87b81419ccb1d6cf16894b7
dbanyaibecker <159194182+dbanyaibecker@users.noreply.github.com>
2024-08-20 14:36:02 -0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -69,8 +69,7 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I’m Daniel Banyai-Becker. I leverage quantitative analysis to transform
-            data into actionable insights, solving ecological and conservation challenges.
+            I’m Daniel, I leverage quantitative analysis to transform data into actionable insights, solving ecological and conservation challenges.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>

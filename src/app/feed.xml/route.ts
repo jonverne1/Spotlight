2ae5ec7664f4a91a27447ce323730428b988a667
Jon Verne <jon.verne@icloud.com>
2025-07-01 13:32:46 -0700

    name change

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: Request) {
   }
 
   let author = {
-    name: 'Spencer Sharp',
+    name: 'Finiva Ednalino',
     email: 'spencer@planetaria.tech',
   }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - Finiva Ednalino',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'Finiva Ednalino - Artist, Designer, and Entrepreneur',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'Finiva Ednalino is an artist, designer, and entrepreneur based in San Diego. Explore my portfolio, blog, and projects that blend creativity with technology.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
